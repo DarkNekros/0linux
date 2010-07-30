ced68fea670ce1fa0d6d8f4304150994f9b4ad8e
appzer0 <appzer0@free.fr>
2010-07-30 19:33:34 +0200

    Modifs dans l'installateur

--- installateur/scripts/media.sh
+++ installateur/scripts/media.sh
@@ -9,43 +9,40 @@ while [ 0 ]; do
 	echo -e "\033[1;32mChoix du média d'installation.\033[0;0m"
 	echo ""
 	echo "Veuillez entrez ci-dessous le média d'installation dont vous"
-	echo "disposez ou bien la méthode d'installation distante souhaitée"
-	echo "pour votre système Linux."
+	echo "disposez ou bien l'emplacement des paquets logiciels que vous"
+	echo "souhaitez utiliser pour installer votre système Linux."
 	echo ""
-	echo "*  Via un média amovible (disque/clé/carte) :"
-	echo "dvd     : vous installez depuis un disque optique"
+	echo "* Installer depuis un média amovible (disque/clé/carte) :"
+	echo "dvd     : vous installez depuis un disque optique (CD/DVD)"
 	echo "usb     : vous installez depuis une clé USB/une carte mémoire"
 	echo ""
-	echo "*  Via un dépôt de paquets (local ou distant) :"
+	echo "* Installer via un dépôt de paquets (local ou distant) :"
 	echo "distant : les paquets sont sur un dépôt distant"
-	echo "local   : les paquets sont dans un répertoire/volume local"
+	echo "local   : les paquets sont dans un répertoire ou un volume local"
 	echo ""
 	echo -n "Votre choix : "
 	read MEDIA;
-	# On analyse le choix de l'utilisateur et on exécute le script ad-hoc en conséquence :
-case "$MEDIASOURCE" in
-	${SELECTDVD_LABEL})
-		. $PWD/detection_dvd.sh
+	case "$MEDIA" in
+	"dvd")
+		. $PWD/dvd.sh
+		break
 	;;
-	${SELECTUSB_LABEL})
-		. $PWD/detection_usb.sh
+	"usb")
+		. $PWD/usb.sh
+		break
 	;;
-	${SELECTHDD_LABEL})
-		. $PWD/detection_hdd.sh
-	#;;
-	#${SELECTNFS_LABEL})
-	#	detection_nfs.sh
-	#;;
-	#${SELECTFTP_LABEL})
-	#	detection_ftp.sh
-esac
-	if [ "$CHOIXCLAVIER" = "" ]; then
-		CHOIXCLAVIER="us"
-	fi
-	
-	# On charge le clavier :
-	loadkeys ${CHOIXCLAVIER} 1> /dev/null 2> /dev/null
-	break
+	"distant")
+		. $PWD/rsync.sh
+		break
+	;;
+	"local")
+		. $PWD/hdd.sh
+		break
+	*)
+		echo "Veuillez entrer un média ou un emplacement valide."
+		sleep 2
+		continue
+	esac
 done
 
 # C'est fini !
