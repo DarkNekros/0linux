1af13f66021ef104515148f94302c07333bbe53a
appzer0 <appzer0@free.fr>
2010-09-26 21:55:07 +0200

    base-systeme : correction du script '0ldd.sh' :(

--- base/base-systeme/0ldd.sh
+++ base/base-systeme/0ldd.sh
@@ -4,15 +4,11 @@ mkdir -p ${PKG}/var/log/deps
 echo "Calcul des dépendances en cours..."
 # Pour chaque fichier exécutable :
 for x in $(find ${PKG} -type f -executable); do
-	# On extrait chaque nom de bibliothèque en dépendance :
-	ldd ${x} 2>/dev/null | grep "=>" | cut -d' ' -f3 | while read plop; do
-		# On se débarrasse de libc puis on découpe pour ne retirer que le nom du paquet
-		# sans sa version :
-		grep $(echo ${plop} | sed -e 's/^\///' -e 's/^lib.*\/libc.*$//') /var/log/paquets/* \
-			| sed -e 's@/var/log/paquets/@@' \
-			      -e 's@:.*$@@' \
-			      -e 's/\(.*\)-[^-][^-]*-[^-][^-]*-[^-][^-]*$/\1/p' -n
+	# On extrait chaque nom de bibliothèque en dépendance en supprimant la racine :
+	ldd ${x} 2>/dev/null | grep "=>" | cut -d' ' -f3 | sed 's/^\///' | while read plop; do
+		# On cherche les dépendances trouvées dans la base des paquets installés et on
+		# découpe pour ne retirer que le nom du paquet sans sa version :
+		grep ${plop} /var/log/paquets/* | sed -e 's/\/var\/log\/paquets\/\(.*\)-[^-][^-]*-[^-][^-]*-[^-][^-]*$/\1/p' -n
 	done
-# On supprime le paquet courant et la glibc ainsi que les doublons, puis on enregistre :
+# On supprime le paquet courant et la glibc en triant les doublons et on enregistre :
 done | grep -v "$NAMETGZ" | grep -v "eglibc"  | sort -u -t ' ' > ${PKG}/var/log/deps/${NAMETGZ}-${VERSION}-${ARCH}-$BUILD
-
