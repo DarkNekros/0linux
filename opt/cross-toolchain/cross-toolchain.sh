fcd8a965290a73a8f8560e85e75402cea3a29293
appzer0 <appzer0@free.fr>
2010-10-08 12:13:20 +0200

    Mise à niveau de la chaîne d'outils croisée pour plus tard.

--- opt/cross-toolchain/cross-toolchain.sh
+++ opt/cross-toolchain/cross-toolchain.sh
@@ -1,71 +1,143 @@
 #!/bin/env bash
 # Construction de la chaine d'outils croisee multilib i686/x86_64.
-# Il faut avoir cree les liens /tools et /cross-tools pointant vers les bons
-# repertoires. On doit avoir cree les repertoires sources, tools, cross-tools
-# dans un repertoire dedie (une partition idealement).
 
-set -xe
+# 1. Choisissez un repertoire et affectez-le a $CLFS, voir plus bas.
+
+# 2. Il faut avoir cree les liens /tools et /cross-tools (à la racine, donc
+# en root), pointant vers les bons repertoires.
+
+# 3. Executer avec la variable DOWNLOAD positionnee pour telecharger les sources :
+#	$ DOWNLOAD=oui ./cross-toolchain.sh
+
+# N.B. : A VOS RISQUES ET PERILS. EVITEZ DE LANCER CECI EN ROOT.
+
+set -e
+set +h
 umask 022
 CWD=$(pwd)
 unset CC CXX AR AS RANLIB LD STRIP CFLAGS CXXFLAGS
 
-# La partition separee ou on va placer les fichiers compiles
-CLFS=${CLFS:-/0-prod}
+# Le PATH placant 'cross-tools' en premier :
+PATH=/cross-tools/bin:/bin:/usr/bin
+export PATH
 
-# L'hote *dinstinct* de l'hote reel
+# La partition separee ou le repertoire ou on va placer les fichiers compiles :
+CLFS=${CLFS:-/home/appzer0/cross-toolchain2011}
+mkdir -p ${CLFS}
+
+# On se garde un journal :
+exec 2>&1 | tee ${CLFS}.log
+
+# L'hote *dinstinct* de l'hote reel :
 CLFS_HOST=${CLFS_HOST:-$(echo ${MACHTYPE} | sed "s/$(echo ${MACHTYPE} | cut -d- -f2)/cross/")}
 
-# Le systeme cible 32 bits
+# Le systeme cible 32 bits :
 CLFS_TARGET32=${CLFS_TARGET32:-i686-0-linux-gnu}
 
-# Le systeme cible 64 bits
+# Le systeme cible 64 bits :
 CLFS_TARGET=${CLFS_TARGET:-x86_64-0-linux-gnu}
 
-# La locale (C ou POSIX et pas autre chose)
+# La locale (C ou POSIX et pas autre chose) :
 export LC_ALL=${LC_ALL:-POSIX}
 
-# Les drapeaux de compilation 32 et 64 bits
+# Les drapeaux de compilation 32 et 64 bits :
 BUILD32=${BUILD32:--m32}
 BUILD64=${BUILD64:--m64}
 
-# Emplacement des archives sources et des correctifs
-SOURCES=${SOURCES:-$CWD/sources}
+# Emplacement des archives sources et des correctifs :
+SOURCES=${SOURCES:-${CLFS}/sources}
+mkdir -p ${SOURCES}
 
-LINUX=${LINUX:-linux-2.6.32.2}
-FILE=${FILE:-file-5.03}
-NCURSES=${NCURSES:-ncurses-5.7}
-GMP=${GMP:-gmp-4.3.1}
-MPFR=${MPFR:-mpfr-2.4.2}
-FINDUTILS=${FINDUTILS:-findutils-4.4.2}
-BINUTILS=${BINUTILS:-binutils-2.20}
-GCC=${GCC:-gcc-4.4.2}
-EGLIBC=${EGLIBC:-eglibc-2.11_20100103}
-PPL=${PPL:-ppl-0.10.2}
-CLOOGPPL=${CLOOGPPL:-cloog-ppl-0.15.7}
-TCL=${TCL:-tcl8.5.8}
-EXPECT=${EXPECT:-expect-5.43.0}
-DEJAGNU=${DEJAGNU:-dejagnu-1.4.4}
-BISON=${BISON:-bison-2.4.1}
-BASH4=${BASH4:-bash-4.1}
-ZLIB=${ZLIB:-zlib-1.2.3}
-BZIP2=${BZIP2:-bzip2-1.0.5}
-COREUTILS=${COREUTILS:-coreutils-8.2}
-DIFFUTILS=${DIFFUTILS:-diffutils-2.8.7}
-FLEX=${FLEX:-flex-2.5.35}
-GAWK=${GAWK:-gawk-3.1.7}
-GETTEXT=${GETTEXT:-gettext-0.17}
-GREP=${GREP:-grep-2.5.4}
-GZIP=${GZIP:-gzip-1.3.13}
-M4=${M4:-m4-1.4.13}
-MAKE=${MAKE:-make-3.81}
-PATCH=${PATCH:-patch-2.6}
-PERL=${PERL:-perl-5.10.1}
-SED=${SED:-sed-4.2.1}
-TAR=${TAR:-tar-1.22}
-TEXINFO=${TEXINFO:-texinfo-4.13a}
-VIM72=${VIM72:-vim-7.2}
-XZ=${XZ:-xz-4.999.9beta}
-UTILLINUXNG=${UTILLINUXNG:-util-linux-ng.2.16.2}
+# Nom et versions des archives :
+LINUX=linux-2.6.35.7
+FILE=file-5.03
+NCURSES=ncurses-5.7
+GMP=gmp-5.0.1
+MPFR=mpfr-3.0.1
+FINDUTILS=findutils-4.4.2
+BINUTILS=binutils-2.20.1
+GCC=gcc-4.5.1
+EGLIBC=eglibc-2.12_10102010
+PPL=ppl-0.11
+CLOOGPPL=cloog-ppl-0.15.9
+TCL=tcl8.5.9
+EXPECT=expect-5.44.1.15
+DEJAGNU=dejagnu-1.4.4
+BISON=bison-2.4.3
+BASH4=bash-4.1
+ZLIB=zlib-1.2.5
+BZIP2=bzip2-1.0.6
+COREUTILS=coreutils-8.5
+DIFFUTILS=diffutils-3.0
+FLEX=flex-2.5.35
+GAWK=gawk-3.1.8
+GETTEXT=gettext-0.18.1.1
+GREP=grep-2.7
+GZIP=gzip-1.4
+M4=m4-1.4.15
+MAKE=make-3.82
+PATCH=patch-2.6.1
+SED=sed-4.2.1
+TAR=tar-1.23
+TEXINFO=texinfo-4.13a
+XZ=xz-4.999.9beta
+UTILLINUXNG=util-linux-ng-2.18
+
+# Si $DOWNLOAD n'est pas vide, alors on veut telecharger les sources : :
+if [ ! "$DOWNLOAD" = "" ]; then
+
+	# URL des archives a telecharger, sources et correctifs :
+	URLS="
+	http://www.kernel.org/pub/linux/kernel/v2.6/$LINUX.tar.bz2
+	ftp://ftp.astron.com/pub/file/$FILE.tar.gz
+	http://patches.cross-lfs.org/dev/file-5.03-cross_compile-1.patch
+	http://ftp.gnu.org/gnu/ncurses/$NCURSES.tar.gz
+	http://ftp.gnu.org/gnu/gmp/$GMP.tar.bz2
+	http://www.mpfr.org/$MPFR/$MPFR.tar.xz
+	ftp://ftp.gnu.org/gnu/findutils/$FINDUTILS.tar.gz
+	ftp://ftp.gnu.org/gnu/binutils/$BINUTILS.tar.bz2
+	http://ftp.gnu.org/gnu/gcc/$GCC/$GCC.tar.bz2
+	http://patches.cross-lfs.org/dev/eglibc-2.12-20100725-r11059-make382-1.patch
+	ftp://ftp.cs.unipr.it/pub/ppl/releases/$(echo $PPL | cut -d'-' -f2)/$PPL.tar.bz2
+	ftp://gcc.gnu.org/pub/gcc/infrastructure/$CLOOGPPL.tar.gz
+	http://prdownloads.sourceforge.net/tcl/$TCL-src.tar.gz
+	http://prdownloads.sourceforge.net/expect/$EXPECT.tar.bz2
+	ftp://ftp.gnu.org/gnu/dejagnu/$DEJAGNU.tar.gz
+	ftp://ftp.gnu.org/gnu/bison/$BISON.tar.bz2
+	http://ftp.gnu.org/gnu/bash/$BASH4.tar.gz
+	http://patches.cross-lfs.org/dev/bash-4.1-branch_update-1.patch
+	http://prdownloads.sourceforge.net/libpng/$ZLIB.tar.bz2
+	http://www.bzip.org/$(echo $BZIP2 | cut d'-' -f2)/$BZIP2.tar.gz
+	ftp://ftp.gnu.org/gnu/coreutils/$COREUTILS.tar.xz
+	ftp://ftp.gnu.org/gnu/diffutils/$DIFFUTILS.tar.xz
+	http://prdownloads.sourceforge.net/flex/$FLEX.tar.bz2
+	http://patches.cross-lfs.org/dev/flex-2.5.35-gcc44-1.patch
+	http://ftp.gnu.org/gnu/gawk/$GAWK.tar.xz
+	http://ftp.gnu.org/gnu/gettext/$GETTEXT.tar.gz
+	http://ftp.gnu.org/gnu/grep/$GREP.tar.gz
+	http://ftp.gnu.org/gnu/gzip/$GZIP.tar.xz
+	http://ftp.gnu.org/gnu/m4/$M4.tar.xz
+	http://ftp.gnu.org/gnu/make/$MAKE.tar.bz2
+	http://ftp.gnu.org/gnu/patch/$PATCH.tar.xz
+	http://ftp.gnu.org/gnu/sed/$SED.tar.bz2
+	http://ftp.gnu.org/gnu/tar/$TAR.tar.bz2
+	http://www.linuxfromscratch.org/patches/lfs/development/tar-1.23-overflow_fix-1.patch
+	ftp://ftp.gnu.org/gnu/texinfo/texinfo-4.13a.tar.gz
+	http://tukaani.org/xz/$XZ.tar.xz
+	http://www.kernel.org/pub/linux/utils/util-linux-ng/v2.18/$UTILINUXNG.tar.bz2"
+	
+	for archive in ${URLS}; do
+		# On telecharge les sources :
+		cd $SOURCES
+		wget -vc ${archive}
+		cd -
+	done
+	
+	# On synchronise avec le SVN de eglibc :
+	svn co svn://svn.eglibc.org/trunk $EGLIBC
+	find . -type d -name ".svn" -delete
+	tar cfvJ $EGLIBC.tar.xz $EGLIBC
+fi
 
 # linux-headers
 cd $SOURCES
@@ -83,6 +155,7 @@ cd $SOURCES
 rm -rf $FILE
 tar xf $CWD/../file/$FILE.tar.*
 cd $SOURCES/$FILE
+cat $SOURCES/file-5.03-cross_compile-1.patch | patch -p1
 ./configure --prefix=/cross-tools
 make
 make install
@@ -92,7 +165,6 @@ cd $SOURCES
 rm -rf $NCURSES
 tar xf $CWD/../ncurses/$NCURSES.tar.*
 cd $SOURCES/$NCURSES
-cat $SOURCES/ncurses-5.7-bash_fix-1.patch | patch -p1
 ./configure --prefix=/cross-tools \
 	--without-debug --without-shared
 make -C include
@@ -114,7 +186,6 @@ cd $SOURCES
 rm -rf $MPFR
 tar xf $CWD/../mpfr/$MPFR.tar.*
 cd $SOURCES/$MPFR
-cat $SOURCES/mpfr-2.4.1-branch_update-2.patch | patch -p1
 LDFLAGS="-Wl,-rpath,/cross-tools/lib" \
 	./configure --prefix=/cross-tools \
 	--enable-shared --with-gmp=/cross-tools
@@ -154,7 +225,6 @@ rm -rf binutils-build
 tar xf $CWD/../binutils/$BINUTILS.tar.*
 rm -rf $SOURCES/binutils-build
 cd $SOURCES/$BINUTILS
-cat $SOURCES/binutils-2.19.51-genscripts_multilib-1.patch | patch -p1
 mkdir -p $SOURCES/binutils-build
 cd $SOURCES/binutils-build
 AR=ar AS=as $SOURCES/$BINUTILS/configure --prefix=/cross-tools \
@@ -172,8 +242,7 @@ rm -rf $GCC
 rm -rf gcc-build
 tar $CWD/../gcc/xf $GCC.tar.*
 cd $SOURCES/$GCC
-cat $SOURCES/gcc-4.4.2-specs-1.patch | patch -p1
-cat $SOURCES/gcc-4.4.2-branch_update-1.patch | patch -p1
+cat $SOURCES/gcc-4.5.1-specs-1.patch | patch -p1
 echo -en '#undef STANDARD_INCLUDE_DIR\n#define STANDARD_INCLUDE_DIR "/tools/include/"\n\n' >> gcc/config/linux.h
 echo -en '\n#undef STANDARD_STARTFILE_PREFIX_1\n#define STANDARD_STARTFILE_PREFIX_1 "/tools/lib/"\n' >> gcc/config/linux.h
 echo -en '\n#undef STANDARD_STARTFILE_PREFIX_2\n#define STANDARD_STARTFILE_PREFIX_2 ""\n' >> gcc/config/linux.h
@@ -200,6 +269,7 @@ rm -rf ${EGLIBC}
 rm -rf eglibc-build
 tar xf $CWD/../eglibc/${EGLIBC}.tar.*
 cd $SOURCES/${EGLIBC}/libc
+cat $SOURCES/eglibc-2.12-20100725-r11059-make382-1.patch | patch -p1
 sed -i 's/$libc_cv_gnu89_inline = yes/"$libc_cv_gnu89_inline" != no/' configure.in
 cp Makeconfig{,.orig}
 sed -e 's/-lgcc_eh//g' Makeconfig.orig > Makeconfig # supression du lien avec libgcc_eh
@@ -230,6 +300,7 @@ rm -rf ${EGLIBC}
 rm -rf eglibc-build
 tar xf $CWD/../eglibc/${EGLIBC}.tar.*
 cd $SOURCES/${EGLIBC}/libc
+cat $SOURCES/eglibc-2.12-20100725-r11059-make382-1.patch | patch -p1
 sed -i 's/$libc_cv_gnu89_inline = yes/"$libc_cv_gnu89_inline" != no/' configure.in
 cp Makeconfig{,.orig}
 sed -e 's/-lgcc_eh//g' Makeconfig.orig > Makeconfig # supression du lien avec libgcc_eh
@@ -260,8 +331,7 @@ rm -rf $GCC
 rm -rf gcc-build
 tar xf $CWD/../gcc/$GCC.tar.*
 cd $SOURCES/$GCC
-cat $SOURCES/gcc-4.4.2-specs-1.patch | patch -p1
-cat $SOURCES/gcc-4.4.2-branch_update-1.patch | patch -p1
+cat $SOURCES/gcc-4.5.1-specs-1.patch | patch -p1
 echo -en '#undef STANDARD_INCLUDE_DIR\n#define STANDARD_INCLUDE_DIR "/tools/include/"\n\n' >> gcc/config/linux.h
 echo -en '\n#undef STANDARD_STARTFILE_PREFIX_1\n#define STANDARD_STARTFILE_PREFIX_1 "/tools/lib/"\n' >> gcc/config/linux.h
 echo -en '\n#undef STANDARD_STARTFILE_PREFIX_2\n#define STANDARD_STARTFILE_PREFIX_2 ""\n' >> gcc/config/linux.h
@@ -307,7 +377,6 @@ cd $SOURCES
 rm -rf $MPFR
 tar xf $CWD/../mpfr/$MPFR.tar.*
 cd $SOURCES/$MPFR
-cat $SOURCES/mpfr-2.4.1-branch_update-2.patch | patch -p1
 CC="${CC} ${BUILD64}" ./configure --prefix=/tools \
 	--build=${CLFS_HOST} --host=${CLFS_TARGET} \
 	--libdir=/tools/lib64 --enable-shared
@@ -375,8 +444,7 @@ rm -rf $GCC
 rm -rf gcc-build
 tar xf $CWD/../gcc/$GCC.tar.*
 cd $SOURCES/$GCC
-cat $SOURCES/gcc-4.4.2-specs-1.patch | patch -p1
-cat $SOURCES/gcc-4.4.2-branch_update-1.patch | patch -p1
+cat $SOURCES/gcc-4.5.1-specs-1.patch | patch -p1
 echo -en '#undef STANDARD_INCLUDE_DIR\n#define STANDARD_INCLUDE_DIR "/tools/include/"\n\n' >> gcc/config/linux.h
 echo -en '\n#undef STANDARD_STARTFILE_PREFIX_1\n#define STANDARD_STARTFILE_PREFIX_1 "/tools/lib/"\n' >> gcc/config/linux.h
 echo -en '\n#undef STANDARD_STARTFILE_PREFIX_2\n#define STANDARD_STARTFILE_PREFIX_2 ""\n' >> gcc/config/linux.h
@@ -402,7 +470,6 @@ cd $SOURCES
 rm -rf $NCURSES
 tar xf $CWD/../ncurses/$NCURSES.tar.*
 cd $SOURCES/$NCURSES
-cat $SOURCES/ncurses-5.7-bash_fix-1.patch | patch -p1
 CC="${CC} ${BUILD64}" CXX="${CXX} ${BUILD64}" \
 	./configure --prefix=/tools --with-shared \
 	--build=${CLFS_HOST} --host=${CLFS_TARGET} \
@@ -418,6 +485,7 @@ cp -a $CWD/../bash/$BASH4.tar.* .
 rm -rf $BASH4
 tar xf $SOURCES/$BASH4.tar.*
 cd $SOURCES/$BASH4
+cat $SOURCES/bash-4.1-branch_update-1.patch | patch -p0
 # De nombreux tests ne comprennent pas la compilation croisee
 cat > config.cache << "EOF"
 ac_cv_func_mmap_fixed_mapped=yes
@@ -636,6 +704,7 @@ cd $SOURCES
 rm -rf $TAR
 tar xf $CWD/../tar/$TAR.tar.*
 cd $SOURCES/$TAR
+ cat $SOURCES/tar-1.23-overflow_fix-1.patch | patch -p1
 cat > config.cache << EOF
 gl_cv_func_wcwidth_works=yes
 gl_cv_func_btowc_eof=yes
