428938bb71fd2cf62488920f8e088c19bbc912c1
appzer0 <appzer0@free.fr>
2012-01-21 18:03:36 +0100

    mozilla-firefox : corrections

---
+++ apps/mozilla-firefox/mozilla-firefox/vendor.js
@@ -0,0 +1,6 @@
+// On utilise la langue système via LANG :
+pref("intl.locale.matchOS", true);
+
+// On désactive la demande de navigateur par défaut :
+pref("browser.shell.checkDefaultBrowser", false);
+
