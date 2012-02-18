16cda77175b0dadb6a14f88c4b76ac26715a5341
appzer0 <appzer0@free.fr>
2012-02-18 20:57:34 +0100

    mozilla-firefox 10.0.2

--- apps/mozilla-firefox/mozilla-firefox/vendor.js
+++ apps/mozilla-firefox/mozilla-firefox/vendor.js
@@ -1,6 +1,7 @@
-// On utilise la langue système via LANG :
-pref("intl.locale.matchOS", true);
-
 // On désactive la demande de navigateur par défaut :
 pref("browser.shell.checkDefaultBrowser", false);
 
+// On désactive la mise à jour automatique :
+pref("app.auto.update", false);
+pref("app.update.auto", false);
+pref("app.update.enabled", false);
