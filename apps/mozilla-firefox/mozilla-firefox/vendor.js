da129ca9ba1dcf598c31b4567563d76ff1c79d1e
appzer0 <appzer0@free.fr>
2013-08-07 13:06:36 +0200

    Nettoyage par le vide pour le retour de 0Linux multilib

--- apps/mozilla-firefox/mozilla-firefox/vendor.js
+++
@@ -1,7 +0,0 @@
-// On désactive la demande de navigateur par défaut :
-pref("browser.shell.checkDefaultBrowser", false);
-
-// On désactive la mise à jour automatique :
-pref("app.auto.update", false);
-pref("app.update.auto", false);
-pref("app.update.enabled", false);
--- apps/mozilla-thunderbird/mozilla-thunderbird/vendor.js
+++
@@ -1,5 +0,0 @@
-// On désactive la mise à jour automatique :
-pref("app.auto.update", false);
-pref("app.update.auto", false);
-pref("app.update.enabled", false);
-
