8a1236419ab4af98a4f7472975e3ba63a4c07170
appzer0 <appzer0@free.fr>
2013-09-15 18:33:51 +0200

    0outils : correction de l'installateur

--- base/0outils/installateur/scripts/installer.sh
+++ base/0outils/installateur/scripts/installer.sh
@@ -26,21 +26,20 @@ else
 	echo -e "\033[1;32mInstallation des paquets.\033[0;0m"
 	echo ""
 	echo "Les paquets du système 0Linux vont maintenant être installés sur votre"
-	echo "partition '$(cat $TMP/partition_racine)'"
+	echo "partition '$(cat $TMP/partition_racine)'."
 	echo ""
 	echo -n "Appuyez sur ENTRÉE pour continuer."
 	read BLURB;
-
+	
 	# On installe d'abord les paquets vitaux de 'base/':
-	for paq in busybox* \
-	base-systeme* \
-	eglibc* \
-	readline* \
-	ncurses* \
-	bash* \
-	sgml*; do
-		spackadd --about ${MEDIACHOISI}/base/${paq}.spack
-		spackadd --root=${SETUPROOT} ${MEDIACHOISI}/base/${paq}.spack &>/dev/null 2>&1
+	for paq in busybox-* \
+	base-systeme-* \
+	glibc-* \
+	readline-* \
+	ncurses-* \
+	bash-* \
+	sgml-*; do
+		spackadd --root=${SETUPROOT} ${MEDIACHOISI}/base/${paq}.spack 2>/dev/null
 	done
 	
 	# On installe tout le reste de 'base/' 'opt/' et xorg/  sauf linux* et les paquets
@@ -48,34 +47,32 @@ else
 	for subdir in base opt xorg; do
 		for paquet in $(find ${MEDIACHOISI}/${subdir} -type f \( \
 		-name "*.spack" \
-		\! -name "base-systeme*" \
-		\! -name "bash*" \
-		\! -name "busybox*" \
-		\! -name "linux*" \
-		\! -name "ncurses*" \
-		\! -name "ndiswrapper*" \
-		\! -name "readline*" \
-		\! -name "sgml*" \) | sort) ; do
-			spackadd --about ${paquet}
-			spackadd --root=${SETUPROOT} ${paquet} &>/dev/null 2>&1
+		\! -name "base-systeme-*" \
+		\! -name "bash-*" \
+		\! -name "busybox-*" \
+		\! -name "glibc-*" \
+		\! -name "linux-*" \
+		\! -name "ncurses-*" \
+		\! -name "ndiswrapper-*" \
+		\! -name "readline-*" \
+		\! -name "sgml-*" \) | sort) ; do
+			spackadd --root=${SETUPROOT} ${paquet} 2>/dev/null
 		done
 	done
 	
-	# Un « hack » pour traiter correctement la post-installation de 'base-systeme' : on n'avait
-	# pas 'coreutils' installé, la post-install n'était donc pas traitée et des '*.0nouveau'
-	# traînaient partout. On réinstalle 'base-systeme', c'est inoffensif :
-	spackadd -f --root=${SETUPROOT} ${MEDIACHOISI}/base/base-systeme-*.spack &>/dev/null 2>&1
-
 	# Linux (appel à 'make' en post-installation, donc de
 	# nombreuses dépendances) :
-	spackadd --about ${MEDIACHOISI}/base/linux*.spack
-	spackadd --root=${SETUPROOT} ${MEDIACHOISI}/base/linux*.spack &>/dev/null 2>&1
+	spackadd --root=${SETUPROOT} ${MEDIACHOISI}/base/linux-*.spack 2>/dev/null
 
 	# ndiswrapper (contient un module noyau et un appel à 'depmod'). "|| true" sert au cas
 	# où 'ndiswrapper' serait absent (ça arrive si l'hôte n'a pas la même version du noyau
 	# et que le paquet n'est donc pas encore compilé) :
-	spackadd --about ${MEDIACHOISI}/base/ndiswrapper*.spack || true
-	spackadd --root=${SETUPROOT} ${MEDIACHOISI}/base/ndiswrapper*.spack &>/dev/null 2>&1 || true
+	spackadd --root=${SETUPROOT} ${MEDIACHOISI}/base/ndiswrapper-*.spack 2>/dev/null || true
+	
+	# On nettoie tous les fichiers '*.0nouveau' :
+	for f in $(find ${SETUPROOT}/etc -type f -name "*.0nouveau"); do
+		mv ${f} $(dirname ${f})/$(basename ${f} .0nouveau)
+	done
 fi
 
-# C'est fini !
+# C'est fini.
