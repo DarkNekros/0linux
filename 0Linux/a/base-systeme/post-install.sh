ce70c375b00d500f42a1ea98389df06e439fad62
appzer0 <appzer0@free.fr>
2014-02-21 13:32:01 +0100

    base-systeme : ajout des UID et GID 'colord'

--- 0Linux/a/base-systeme/post-install.sh
+++ 0Linux/a/base-systeme/post-install.sh
@@ -77,6 +77,7 @@ basegid=(
 		"users:100"
 		"console:101"
 		"polkitd:102"
+		"colord:108"
 		"gdm:120"
 		"vboxusers:215"
 		)
@@ -113,6 +114,7 @@ baseuid=(
 		"pop:90:90:Utilisateur POP:/:/usr/bin/false"
 		"nobody:99:99:Utilisateur fictif nobody:/:/usr/bin/false"
 		"polkitd:102:102:Utilisateur Policy Kit:/:/usr/bin/false"
+		"colord:108:108:Utilisateur pour colord:/:/usr/bin/false"
 		"gdm:120:120:Utilisateur GNOME GDM:/var/lib/gdm:/usr/bin/false"
 		"vboxweb:240:215:Utilisateur VirtualBox (Web):/var/lib/vboxweb:/usr/bin/false"
 		)
@@ -149,6 +151,7 @@ messagebus:x:81:81:User for D-BUS:/var/run/dbus:/bin/false
 haldaemon:x:82:82:User for HAL:/var/run/hald:/bin/false
 pop:x:90:90:Utilisateur POP:/:/usr/bin/false
 nobody:x:99:99:Utilisateur fictif nobody:/:/usr/bin/false
+colord:x:108:108:Utilisateur pour colord:/:/usr/bin/false
 vboxweb:x:240:215:Utilisateur VirtualBox (Web):/var/lib/vboxweb:/usr/bin/false
 dovenull:x:74:74:Utiliateur non fiable pour dovecot:/var/empty:/usr/bin/false
 dovecot:x:76:76:Utiliateur dovecot:/var/empty:/usr/bin/false
@@ -207,6 +210,7 @@ nobody:x:98:nobody
 nogroup:x:99:
 users:x:100:
 console:x:101:
+colord:x:108:
 vboxusers:x:215:
 lock:x:54:
 dovenull:x:74:
