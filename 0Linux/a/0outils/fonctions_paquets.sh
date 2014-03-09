dbf2fc3b1a0c7fab87e8b7d4b5c914584d5dcf80
appzer0 <appzer0@free.fr>
2014-03-09 12:39:57 +0100

    0outils 9.8.2 : correction de 'fonctions_paquets.sh' : limitation à 3 des réessais de wget, expiration des DNS à 20 secondes

--- 0Linux/a/0outils/fonctions_paquets.sh
+++ 0Linux/a/0outils/fonctions_paquets.sh
@@ -108,9 +108,11 @@ telecharger_sources() {
 		# On télécharge l'archive source et on retombe sur le FTP de 0linux si le téléchargement se passe mal (fichier
 		# inexistant, erreur du serveur, etc.) :
 		if [ ! -r ${PKGSOURCES}/${NAMETGZ}/$(basename ${wgeturl}) ]; then
-			wget -vc --no-check-certificate ${wgeturl} \
+			wget -vc --tries=3 --dns-timeout=20 --no-check-certificate \
+				${wgeturl} \
 				-O ${PKGSOURCES}/${NAMETGZ}/$(basename ${wgeturl}).part || \
-				wget -vc ftp://ftp.igh.cnrs.fr/pub/os/linux/0linux/archives_sources/${NAMETGZ}/$(basename ${wgeturl}) \
+				wget -vc --tries=3 --dns-timeout=20 \
+					ftp://ftp.igh.cnrs.fr/pub/os/linux/0linux/archives_sources/${NAMETGZ}/$(basename ${wgeturl}) \
 					-O ${PKGSOURCES}/${NAMETGZ}/$(basename ${wgeturl}).part
 			
 			mv ${PKGSOURCES}/${NAMETGZ}/$(basename ${wgeturl}){.part,}
